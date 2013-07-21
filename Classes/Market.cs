57722336430513e2617760a091a3c4e8ae6555e6
austo <moraustin@gmail.com>
2013-07-21 00:19:34 -0400

    added market

---
+++ Classes/Market.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SecuritiesPositionCalculator
+{
+    public class Market
+    {
+        private readonly ReaderWriterLockSlim 
+            _cacheLock = new ReaderWriterLockSlim();
+        private readonly Dictionary<string, double>
+            _securities = new Dictionary<string, double>();
+
+        private int _consoleStartRow;
+        private int _priceStartColumn;
+
+        public double GetPrice(string secId)
+        {
+            _cacheLock.EnterReadLock();
+            try
+            {
+                return _securities[secId];
+            }
+            finally
+            {
+                _cacheLock.ExitReadLock();
+            }
+        }
+
+        public Market(TradeOrder order)
+        {
+            foreach (var trade in order.Trades
+                .Where(trade => !_securities.ContainsKey(trade.SecurityId)))
+            {
+                _securities.Add(trade.SecurityId, trade.TradePrice);
+            }
+        }
+
+        public void Start()
+        {
+            _consoleStartRow = Console.CursorTop + 1;
+            var random = new Random(DateTime.Now.Millisecond);
+            var secNames = _securities.Keys.ToArray();
+            _priceStartColumn =
+                secNames.OrderByDescending(s => s.Length).First().Length + 4;
+            try
+            {
+                while (true)
+                {
+                    _cacheLock.EnterUpgradeableReadLock();
+                    Fluctuate(random, secNames);
+                    _cacheLock.ExitUpgradeableReadLock();
+                    Thread.Sleep(random.Next()%1000); // sleep for somewhere under a second
+                }
+            }
+            catch(ThreadAbortException abEx)
+            {
+                Console.WriteLine(
+                    string.Format("Due to {0}, the market has closed.",
+                    abEx.ExceptionState));
+            }
+        }
+
+        private void Fluctuate(Random random, string[] secNames)
+        {
+            int i = 0, s = _securities.Count;
+            for (; i < s; i++)
+            {
+                var secName = secNames[i];
+                var price = _securities[secName];
+                var flux = price * ((random.NextDouble() % Cfg.Settings.Volitility) / 100);
+                _cacheLock.EnterWriteLock();
+                price += random.Next() % 2 == 0 ? flux : flux * -1;
+                _securities[secName] = price;
+                _cacheLock.ExitWriteLock();
+
+                Console.SetCursorPosition(0, _consoleStartRow + i);
+                Console.Write(string.Format("{0}:", secName));
+                Console.CursorLeft = _priceStartColumn;
+                Console.WriteLine(string.Format("{0:C}", price));
+
+                if (random.Next() % 4 == 0) // simulate acyclic pause
+                {
+                    Thread.Sleep(random.Next() % 100);
+                }
+            }
+        }
+    }
+}
